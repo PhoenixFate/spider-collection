380225f42958e46888e6318b0913f74d224e1eea
phoenixfate <sm516116978@outlook.com>
2021-09-05 00:16:03 +0800

    命名规范

--- spider-feng/docker/start.sh
+++ spider-feng/docker/start.sh
@@ -1,4 +1,3 @@
-cd /root/spider/feng-docker
-sh clear-spider-feng.sh
-sh build-spider-feng-image.sh
-sh run-spider-feng-docker.sh
+sh ./clear-spider-feng.sh
+sh ./build-spider-feng-image.sh
+sh ./run-spider-feng-docker.sh
